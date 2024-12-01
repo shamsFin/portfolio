e57386a288a261fa137e643024eab4bbd6320de9
Shamsul Arefin <msarefins@gmail.com>
2024-12-01 23:07:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+console.log('Its working')
+
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+    setTheme('blue')
+}else{
+    setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+
+for (var i=0; themeDots.length > i; i++){
+    themeDots[i].addEventListener('click', function(){
+        let mode = this.dataset.mode
+        console.log('Option clicked:', mode)
+        setTheme(mode)
+    })
+}
+
+function setTheme(mode){
+    if(mode == 'light'){
+        document.getElementById('theme-style').href = 'default.css'
+    }
+
+    if(mode == 'blue'){
+        document.getElementById('theme-style').href = 'blue.css'
+    }
+
+    if(mode == 'green'){
+        document.getElementById('theme-style').href = 'green.css'
+    }
+
+    if(mode == 'purple'){
+        document.getElementById('theme-style').href = 'purple.css'
+    }
+
+    localStorage.setItem('theme', mode)
+}
+
+// Form Submission to Formspree
+
+window.addEventListener("DOMContentLoaded", function() {
+
+    // get the form elements defined in your form HTML above
+    
+    var form = document.getElementById("contact-form");
+    var status = document.getElementById("status");
+
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+        form.reset();
+        status.innerHTML = "Thanks!";
+    }
+
+    function error() {
+        status.innerHTML = "Oops! There was a problem.";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+        ev.preventDefault();
+        var data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
+    });
+});
+
+//Onclick
+
+function validate(){
+    if ((document.form.name.value=="") || (document.form.email.value=="")){
+        alert("You must fill in all of the required fields!")
+    }
+    else
+     window.location.href='thankYouPage.html';
+}
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+            success(xhr.response, xhr.responseType);
+        } else {
+            error(xhr.status, xhr.response, xhr.responseType);
+        }
+    };
+    xhr.send(data);
+}
